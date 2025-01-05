13eed0f5cb30d181379479d39a0f9ad6fd10438d
ToutLeTresorDuCapitaine <colin.parisod@epfl.ch>
2025-01-05 13:29:07 +0100

    3eme tentative

---
+++ searchindex.en.js
@@ -0,0 +1,282 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "My own website \u003e Personal Projects",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Pure Sine Wave Inverter",
+    "uri": "/personal/pure-sine-wave-inverter/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Personal Projects",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Business Card",
+    "uri": "/personal/business-card/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Personal Projects",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Ball Physics Simulation",
+    "uri": "/personal/ball-physics-simulation/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Personal Projects",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Brawlhalla Without Lag",
+    "uri": "/personal/brawlhalla-without-lag/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Personal Projects",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Mandelbrot Zoom",
+    "uri": "/personal/mandelbrot-zoom/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Mandelbrot Pong",
+    "uri": "/cursus/bachelor/mandelbrot-pong/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "LLC Resonant Converter",
+    "uri": "/cursus/bachelor/llc-resonant-converter/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Boost Converter",
+    "uri": "/cursus/bachelor/boost-converter/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tank Minigame",
+    "uri": "/cursus/bachelor/tank-minigame/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tik Tak Toe",
+    "uri": "/cursus/bachelor/tik-tak-toe/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Random Forest",
+    "uri": "/cursus/bachelor/random-forest/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Nut Cracker",
+    "uri": "/cursus/bachelor/nut-cracker/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Alarm Clock",
+    "uri": "/cursus/bachelor/alarm-clock/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Cursus Projects",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Maturity Project",
+    "uri": "/cursus/maturity-project/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Ant Colony Simulation",
+    "uri": "/cursus/bachelor/ant-colony/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Cursus Projects",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "EPFL Bachelor",
+    "uri": "/cursus/bachelor/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Cursus Projects",
+    "content": "I am not in master yet, stay tuned!",
+    "description": "I am not in master yet, stay tuned!",
+    "tags": [],
+    "title": "EPFL Master",
+    "uri": "/cursus/master/index.html"
+  },
+  {
+    "breadcrumb": "My own website",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Personal Projects",
+    "uri": "/personal/index.html"
+  },
+  {
+    "breadcrumb": "My own website",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Cursus Projects",
+    "uri": "/cursus/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e EPFL Racing Team \u003e Ariane",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "TSAL Logic",
+    "uri": "/epflrt/ariane/tsal-logic/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e EPFL Racing Team \u003e Sirius",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Backbone",
+    "uri": "/epflrt/sirius/backbone/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e EPFL Racing Team \u003e LRT6",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Module Tester",
+    "uri": "/epflrt/lrt6/module-tester/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e EPFL Racing Team \u003e LRT6",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Smart Switch",
+    "uri": "/epflrt/lrt6/smart-switch/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e EPFL Racing Team \u003e LRT6",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "LED Driver",
+    "uri": "/epflrt/lrt6/led-driver/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e EPFL Racing Team \u003e LRT6",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Buck Boost Converter",
+    "uri": "/epflrt/lrt6/buck-boost/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e EPFL Racing Team",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "LRT6",
+    "uri": "/epflrt/lrt6/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e EPFL Racing Team",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Sirius",
+    "uri": "/epflrt/sirius/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e EPFL Racing Team",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Ariane",
+    "uri": "/epflrt/ariane/index.html"
+  },
+  {
+    "breadcrumb": "My own website",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "EPFL Racing Team",
+    "uri": "/epflrt/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e EPFL Racing Team \u003e Ariane",
+    "content": "Here is the BSPD.",
+    "description": "Here is the BSPD.",
+    "tags": [],
+    "title": "Brake System Plausibility Device",
+    "uri": "/epflrt/ariane/bspd/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Welcome on my website!",
+    "description": "Welcome on my website!",
+    "tags": [],
+    "title": "My own website",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "My own website",
+    "content": "Hello my guy",
+    "description": "Hello my guy",
+    "tags": [],
+    "title": "Subdir",
+    "uri": "/subdir/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Subdir",
+    "content": "Is this image showing up? title of image",
+    "description": "Is this image showing up? title of image",
+    "tags": [],
+    "title": "La Vie De Moi",
+    "uri": "/subdir/b/b/index.html"
+  },
+  {
+    "breadcrumb": "My own website",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "My own website",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
