e5d639e71e29d3955eb2f38fb0b14ae8773519e2
ToutLeTresorDuCapitaine <colin.parisod@epfl.ch>
2025-01-08 19:38:21 +0100

    icones menus et shortcuts menu MORE

--- searchindex.en.js
+++ searchindex.en.js
@@ -1,146 +1,313 @@
 var relearn_searchindex = [
   {
-    "breadcrumb": "My own website \u003e Personal Projects",
+    "breadcrumb": "My own website \u003e Personal",
     "content": "",
     "description": "",
     "tags": [],
+    "title": "This Website",
+    "uri": "/personal/website/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Altium",
+    "uri": "/tags/altium/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Power Electronics",
+    "uri": "/tags/power-electronics/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Personal",
+    "content": "",
+    "description": "",
+    "tags": [
+      "Altium",
+      "Power Electronics",
+      "Signal Processing",
+      "Safety"
+    ],
     "title": "Pure Sine Wave Inverter",
     "uri": "/personal/pure-sine-wave-inverter/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Personal Projects",
+    "breadcrumb": "My own website \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Safety",
+    "uri": "/tags/safety/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Signal Processing",
+    "uri": "/tags/signal-processing/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Personal",
     "content": "Gallery\r3D Recto\r3D Verso\rReal Recto\rReal Verso\rVideo Demo\rAuthor(s) Colin Parisod Date December 2024 Skills Altium Designer, NFC Technologies Difficulty 2/10 Current Iteration REV C This project has to fulfill 5 objectives :\nFinancial : It is not intended to distribute them for business opportunities because they are too expensive. Electrical : To exchange informations without giving the card, a NFC tag must be embedded. Mechanical : It must have the same format as a credit card in order to easily fit inside a card holder. Utility : The information present on the card (written, not NFT) must be valid in the long term future. Aesthetics : It should look simple and as clean as possible without impacting other objectives.",
     "description": "Gallery\r3D Recto\r3D Verso\rReal Recto\rReal Verso\rVideo Demo\rAuthor(s) Colin Parisod Date December 2024 Skills Altium Designer, NFC Technologies Difficulty 2/10 Current Iteration REV C This project has to fulfill 5 objectives :",
-    "tags": [],
+    "tags": [
+      "Altium",
+      "Signal Processing"
+    ],
     "title": "Business Card",
     "uri": "/personal/business-card/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Personal Projects",
+    "breadcrumb": "My own website \u003e Personal",
     "content": "",
     "description": "",
-    "tags": [],
+    "tags": [
+      "Python",
+      "Simulation"
+    ],
     "title": "Ball Physics Simulation",
     "uri": "/personal/ball-physics-simulation/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Personal Projects",
+    "breadcrumb": "My own website \u003e Tags",
     "content": "",
     "description": "",
     "tags": [],
+    "title": "Tag :: Python",
+    "uri": "/tags/python/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Simulation",
+    "uri": "/tags/simulation/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Personal",
+    "content": "",
+    "description": "",
+    "tags": [
+      "Python",
+      "Game"
+    ],
     "title": "Brawlhalla Without Lag",
     "uri": "/personal/brawlhalla-without-lag/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Personal Projects",
+    "breadcrumb": "My own website \u003e Tags",
     "content": "",
     "description": "",
     "tags": [],
+    "title": "Tag :: Game",
+    "uri": "/tags/game/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Personal",
+    "content": "",
+    "description": "",
+    "tags": [
+      "Python",
+      "Simulation"
+    ],
     "title": "Mandelbrot Zoom",
     "uri": "/personal/mandelbrot-zoom/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "breadcrumb": "My own website \u003e Academic \u003e EPFL Bachelor",
     "content": "",
     "description": "",
-    "tags": [],
+    "tags": [
+      "VHDL",
+      "Simulation",
+      "Game"
+    ],
     "title": "Mandelbrot Pong",
-    "uri": "/cursus/bachelor/mandelbrot-pong/index.html"
+    "uri": "/academic/bachelor/mandelbrot-pong/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "breadcrumb": "My own website \u003e Tags",
     "content": "",
     "description": "",
     "tags": [],
+    "title": "Tag :: VHDL",
+    "uri": "/tags/vhdl/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Academic \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [
+      "Altium",
+      "Power Electronics",
+      "Signal Processing"
+    ],
     "title": "LLC Resonant Converter",
-    "uri": "/cursus/bachelor/llc-resonant-converter/index.html"
+    "uri": "/academic/bachelor/llc-resonant-converter/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "breadcrumb": "My own website \u003e Academic \u003e EPFL Bachelor",
     "content": "",
     "description": "",
-    "tags": [],
+    "tags": [
+      "Power Electronics"
+    ],
     "title": "Boost Converter",
-    "uri": "/cursus/bachelor/boost-converter/index.html"
+    "uri": "/academic/bachelor/boost-converter/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "breadcrumb": "My own website \u003e Tags",
     "content": "",
     "description": "",
     "tags": [],
+    "title": "Tag :: C (Programming Language)",
+    "uri": "/tags/c-programming-language/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Academic \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [
+      "C (Programming Language)",
+      "Game"
+    ],
     "title": "Tank Minigame",
-    "uri": "/cursus/bachelor/tank-minigame/index.html"
+    "uri": "/academic/bachelor/tank-minigame/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "breadcrumb": "My own website \u003e Tags",
     "content": "",
     "description": "",
     "tags": [],
+    "title": "Tag :: Assembly",
+    "uri": "/tags/assembly/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Academic \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [
+      "Assembly",
+      "Game"
+    ],
     "title": "Tik Tak Toe",
-    "uri": "/cursus/bachelor/tik-tak-toe/index.html"
+    "uri": "/academic/bachelor/tik-tak-toe/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "breadcrumb": "My own website \u003e Academic \u003e EPFL Bachelor",
     "content": "",
     "description": "",
-    "tags": [],
+    "tags": [
+      "Python",
+      "Simulation"
+    ],
     "title": "Random Forest",
-    "uri": "/cursus/bachelor/random-forest/index.html"
+    "uri": "/academic/bachelor/random-forest/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "breadcrumb": "My own website \u003e Tags",
     "content": "",
     "description": "",
     "tags": [],
+    "title": "Tag :: Computer Aided Design",
+    "uri": "/tags/computer-aided-design/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Academic \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [
+      "Computer Aided Design"
+    ],
     "title": "Nut Cracker",
-    "uri": "/cursus/bachelor/nut-cracker/index.html"
+    "uri": "/academic/bachelor/nut-cracker/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "breadcrumb": "My own website \u003e Academic \u003e EPFL Bachelor",
     "content": "",
     "description": "",
-    "tags": [],
+    "tags": [
+      "Logisim Evolution",
+      "Signal Processing"
+    ],
     "title": "Alarm Clock",
-    "uri": "/cursus/bachelor/alarm-clock/index.html"
+    "uri": "/academic/bachelor/alarm-clock/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Cursus Projects",
+    "breadcrumb": "My own website \u003e Tags",
     "content": "",
     "description": "",
     "tags": [],
+    "title": "Tag :: Logisim Evolution",
+    "uri": "/tags/logisim-evolution/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Arduino",
+    "uri": "/tags/arduino/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Academic",
+    "content": "",
+    "description": "",
+    "tags": [
+      "Arduino",
+      "Signal Processing"
+    ],
     "title": "Maturity Project",
-    "uri": "/cursus/maturity-project/index.html"
+    "uri": "/academic/maturity-project/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Cursus Projects \u003e EPFL Bachelor",
+    "breadcrumb": "My own website \u003e Academic \u003e EPFL Bachelor",
+    "content": "",
+    "description": "",
+    "tags": [
+      "C++",
+      "Simulation"
+    ],
+    "title": "Ant Colony Simulation",
+    "uri": "/academic/bachelor/ant-colony/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e Tags",
     "content": "",
     "description": "",
     "tags": [],
-    "title": "Ant Colony Simulation",
-    "uri": "/cursus/bachelor/ant-colony/index.html"
+    "title": "Tag :: C++",
+    "uri": "/tags/c\u0026#43;\u0026#43;/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Cursus Projects",
+    "breadcrumb": "My own website \u003e Academic",
     "content": "",
     "description": "",
     "tags": [],
     "title": "EPFL Bachelor",
-    "uri": "/cursus/bachelor/index.html"
+    "uri": "/academic/bachelor/index.html"
   },
   {
-    "breadcrumb": "My own website \u003e Cursus Projects",
+    "breadcrumb": "My own website \u003e Academic",
     "content": "I am not in master yet, stay tuned!",
     "description": "I am not in master yet, stay tuned!",
     "tags": [],
     "title": "EPFL Master",
-    "uri": "/cursus/master/index.html"
+    "uri": "/academic/master/index.html"
   },
   {
     "breadcrumb": "My own website",
     "content": "",
     "description": "",
     "tags": [],
-    "title": "Personal Projects",
+    "title": "Personal",
     "uri": "/personal/index.html"
   },
   {
@@ -148,14 +315,17 @@ var relearn_searchindex = [
     "content": "",
     "description": "",
     "tags": [],
-    "title": "Cursus Projects",
-    "uri": "/cursus/index.html"
+    "title": "Academic",
+    "uri": "/academic/index.html"
   },
   {
     "breadcrumb": "My own website \u003e EPFL Racing Team \u003e Ariane",
     "content": "",
     "description": "",
-    "tags": [],
+    "tags": [
+      "Altium",
+      "Signal Processing"
+    ],
     "title": "TSAL Logic",
     "uri": "/epflrt/ariane/tsal-logic/index.html"
   },
@@ -163,7 +333,12 @@ var relearn_searchindex = [
     "breadcrumb": "My own website \u003e EPFL Racing Team \u003e Sirius",
     "content": "",
     "description": "",
-    "tags": [],
+    "tags": [
+      "Altium",
+      "Power Electronics",
+      "Signal Processing",
+      "Safety"
+    ],
     "title": "Backbone",
     "uri": "/epflrt/sirius/backbone/index.html"
   },
@@ -171,7 +346,9 @@ var relearn_searchindex = [
     "breadcrumb": "My own website \u003e EPFL Racing Team \u003e LRT6",
     "content": "",
     "description": "",
-    "tags": [],
+    "tags": [
+      "Altium"
+    ],
     "title": "Module Tester",
     "uri": "/epflrt/lrt6/module-tester/index.html"
   },
@@ -179,7 +356,10 @@ var relearn_searchindex = [
     "breadcrumb": "My own website \u003e EPFL Racing Team \u003e LRT6",
     "content": "",
     "description": "",
-    "tags": [],
+    "tags": [
+      "Altium",
+      "Power Electronics"
+    ],
     "title": "Smart Switch",
     "uri": "/epflrt/lrt6/smart-switch/index.html"
   },
@@ -187,7 +367,11 @@ var relearn_searchindex = [
     "breadcrumb": "My own website \u003e EPFL Racing Team \u003e LRT6",
     "content": "",
     "description": "",
-    "tags": [],
+    "tags": [
+      "Altium",
+      "Power Electronics",
+      "Signal Processing"
+    ],
     "title": "LED Driver",
     "uri": "/epflrt/lrt6/led-driver/index.html"
   },
@@ -195,7 +379,11 @@ var relearn_searchindex = [
     "breadcrumb": "My own website \u003e EPFL Racing Team \u003e LRT6",
     "content": "",
     "description": "",
-    "tags": [],
+    "tags": [
+      "Altium",
+      "Power Electronics",
+      "Signal Processing"
+    ],
     "title": "Buck Boost Converter",
     "uri": "/epflrt/lrt6/buck-boost/index.html"
   },
@@ -235,9 +423,21 @@ var relearn_searchindex = [
     "breadcrumb": "My own website \u003e EPFL Racing Team \u003e Ariane",
     "content": "Here is the BSPD.",
     "description": "Here is the BSPD.",
-    "tags": [],
+    "tags": [
+      "Altium",
+      "Signal Processing",
+      "Safety"
+    ],
     "title": "Brake System Plausibility Device",
     "uri": "/epflrt/ariane/bspd/index.html"
+  },
+  {
+    "breadcrumb": "My own website \u003e ",
+    "content": "Voila les credits",
+    "description": "Credits for the tools and services used for this website.",
+    "tags": [],
+    "title": "Credits",
+    "uri": "/more/credits/index.html"
   },
   {
     "breadcrumb": "",
@@ -250,17 +450,17 @@ var relearn_searchindex = [
   {
     "breadcrumb": "My own website",
     "content": "",
-    "description": "",
+    "description": "Search by category",
     "tags": [],
-    "title": "Categories",
-    "uri": "/categories/index.html"
+    "title": "Tags",
+    "uri": "/tags/index.html"
   },
   {
     "breadcrumb": "My own website",
     "content": "",
     "description": "",
     "tags": [],
-    "title": "Tags",
-    "uri": "/tags/index.html"
+    "title": "Categories",
+    "uri": "/categories/index.html"
   }
 ]
